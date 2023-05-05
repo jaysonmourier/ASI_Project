ed2ddcad1e5e941cd541d1807476fe7de838fcc2
cdemiribel <clement.demiribel@gmail.com>
2023-05-05 15:06:10 +0200

    fin affichage de tous les articles

--- angular/front_angular/src/app/disque/disque.component.ts
+++ angular/front_angular/src/app/disque/disque.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Article} from "../Modele/article.modele";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-disque',
   templateUrl: './disque.component.html',
   styleUrls: ['./disque.component.css']
 })
-export class DisqueComponent {
+export class DisqueComponent implements OnInit {
+  articles: Article[] = [];
 
+  constructor(private http: HttpClient) {
+    console.log("loaded");
+  }
+
+  ngOnInit() {
+    this.http.get<Article[]>('http://localhost:8080/ASI_Project_war/api/articles/category/21')
+      .subscribe((response: Article[]) => {
+        this.articles = response;
+      });
+  }
 }
--- angular/front_angular/src/app/stock-accessoires/stock-accessoires.component.ts
+++ angular/front_angular/src/app/stock-accessoires/stock-accessoires.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {Article} from "../Modele/article.modele";
+import {HttpClient} from "@angular/common/http";
 @Component({
   selector: 'app-stock-accessoires',
   templateUrl: './stock-accessoires.component.html',
   styleUrls: ['./stock-accessoires.component.css']
 })
-export class StockAccessoiresComponent {
+export class StockAccessoiresComponent implements OnInit {
+  articles: Article[] = [];
 
+  constructor(private http: HttpClient) {
+    console.log("loaded");
+  }
+
+  ngOnInit() {
+    this.http.get<Article[]>('http://localhost:8080/ASI_Project_war/api/articles/category/23')
+      .subscribe((response: Article[]) => {
+        this.articles = response;
+      });
+  }
 }
