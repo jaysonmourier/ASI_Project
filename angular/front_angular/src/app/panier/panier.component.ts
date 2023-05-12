1ca7a7d0b0683b4cc1f0ccc62acc8e298b7688ae
cdemiribel <clement.demiribel@gmail.com>
2023-05-12 14:14:59 +0200

    validation panier

--- angular/front_angular/src/app/panier/panier.component.ts
+++ angular/front_angular/src/app/panier/panier.component.ts
@@ -46,4 +46,35 @@ export class PanierComponent {
     });
   }
 
+  viderPanier() {
+    // Boucler sur tous les articles du panier et les supprimer individuellement
+    for (let article of this.articles) {
+      this.http.delete('http://localhost:8080/ASI_Project_war/api/panier/retirer/' + article.id).subscribe(() => {
+        this.actualiserPanier();
+      }, error => {
+        console.error(error);
+      });
+    }
+  }
+
+  private actualiserPanier() {
+    this.articles = [];
+    this.quantities = {};
+    this.total = 0;
+    this.http.get<{ total: number, articles: { [key: number]: number } }>('http://localhost:8080/ASI_Project_war/api/panier')
+      .subscribe(response => {
+        console.log(response);
+        for (let articleId in response.articles) {
+          this.http.get<Article>('http://localhost:8080/ASI_Project_war/api/articles/' + articleId)
+            .subscribe((data: Article) => {
+              console.log(data);
+              this.articles.push(data);
+              let quantity = response.articles[articleId];
+              this.quantities[data.id] = quantity;
+              this.total += data.price * quantity;
+            });
+        }
+      });
+}
+
 }
